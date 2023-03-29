71ef150824229a488f0a6242bc1afb648256f5be
hyungeun.jin <kingwang09@gmail.com>
2023-03-29 17:20:47 +0900

    new macbook git config setting.

--- src/test/java/org/jesus/shop/ShopApplicationTests.java
+++ src/test/java/org/jesus/shop/ShopApplicationTests.java
@@ -1,13 +1,16 @@
 package org.jesus.shop;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Slf4j
 @SpringBootTest
 class ShopApplicationTests {
 
 	@Test
 	void contextLoads() {
+		log.debug("new macbook git settings...");
 	}
 
 }
